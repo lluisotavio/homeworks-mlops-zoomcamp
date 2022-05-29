3538677729ca733928988b75199b3c89ff94d3fd
lluisotavio <lluisotavio@gmail.com>
2022-05-29 15:27:44 -0300

    add homework 02

---
+++ 02/train.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+import pickle
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+
+import mlflow
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+def run(data_path):
+
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment("ny-green-taxi-experiment-HW")
+
+    with mlflow.start_run():
+        mlflow.sklearn.autolog()
+        
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+
+        
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        mlflow.sklearn.log_model(rf, artifact_path='artifact')
+        print(f"default artifacts URI: '{mlflow.get_artifact_uri()}'")
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="./output",
+        help="the location where the processed NYC taxi trip data was saved."
+    )
+    args = parser.parse_args()
+
+    run(args.data_path)
